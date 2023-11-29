546c259d247a480f88acd314cc9772db534e2ee0
Vlad Emelianov <volshebnyi@gmail.com>
2023-11-29 07:04:07 +0300

    Update helpers

--- .github/workflows/helpers.js
+++ .github/workflows/helpers.js
@@ -298,6 +298,7 @@ module.exports = {
     extractVersions,
     extractDownloadLinks,
     extractAioBotocoreVersions,
+    getAioBoto3Version,
     getAioBotocoreVersion,
     getTypesAioBotocoreVersions,
     extractAioBotocoreDownloadLinks,
